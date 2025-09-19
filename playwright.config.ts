03260ac26b4f2ecafcc0e1d4c9bd95497c7a8c63
M Abbas Khan <ak.mohmnd@gmail.com>
2025-09-19 10:25:57 +0500

    Add: Configuration file and folder structure

---
+++ playwright.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  use: {
+    baseURL: "https://www.saucedemo.com",
+    headless: true,
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+  },
+  reporter: [["list"], ["html", { outputFolder: "playwright-report" }]],
+});
---
+++ utils/testdata.ts
@@ -0,0 +1,10 @@
+export const credentials = {
+  username: "standard_user",
+  password: "secret_sauce",
+};
+
+export const checkoutInfo = {
+  firstName: "John",
+  lastName: "Doe",
+  postalCode: "12345",
+};
